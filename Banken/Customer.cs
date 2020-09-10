45d335e0478a6361828227bec3c1e913d931af01
Theo Holmgren <theokodar@gmail.com>
2020-09-10 10:25:27 +0200

    Add project files.

---
+++ Banken/Customer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banken
+{
+    class Customer
+    {
+        public string Name { get; set; }
+        public int Balance { get; set; }
+
+        public string ShowCustomer() 
+        {
+            return Name + "with balance: " + Balance; 
+        }
+    }
+}
---
+++ Banken/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banken
+{
+    class Program
+    {
+        static List<int> Customers = new List<int>();
+        static void Main(string[] args)
+
+        switch(choice)
+
+        {
+            case 1:
+
+                break;
+        }
+
+        {
+            SelectMenuItem();
+        }
+
+        private static void SelectMenuItem()
+        {
+            Console.WriteLine("1 : Lägg till en användare");
+            Console.WriteLine("2 : Ta bort en användare");
+            Console.WriteLine("3 : Visa alla befintliga användare");
+            Console.WriteLine("4 : Visa saldo för en användare");
+            Console.WriteLine("5 : Gör en insättning för en användare");
+            Console.WriteLine("6 : Gör ett uttag för en användare");
+            Console.WriteLine("7 : Avsluta programmet");
+            Console.WriteLine("Skriv ditt val: ");
+
+            int choice = int.Parse(Console.ReadLine());
+        }
+    }
+}
